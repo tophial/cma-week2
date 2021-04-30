f40170b152ac11fb333e9e0882af4b52693255f7
Alissa <alissa.tophinke@hotmail.com>
2021-04-30 11:20:22 +0200

    task 0 complete

--- exercise2-week2.R
+++ exercise2-week2.R
@@ -1,2 +1,17 @@
 #exercise 2 - Alissa Tophinke
 
+#Task 0: Import your data
+## Load the necessary libraries ################################################
+
+library(readr)        # to import tabular data (e.g. csv)
+library(dplyr)        # to manipulate (tabular) data
+library(ggplot2)      # to visualize data
+library(sf)           # to handle spatial vector data
+library(terra)        # To handle raster data
+library(lubridate)    # To handle dates and times
+
+## Import the downloaded csv ##################################################
+
+wildschwein_BE <- read_delim("wildschwein_BE_2056.csv",",") # adjust path
+
+wildschwein_BE <- st_as_sf(wildschwein_BE, coords = c("E", "N"), crs = 2056, remove = FALSE)
