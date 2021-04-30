59d6ee76e2ee108a1aec62a1796accf8d19b954f
Alissa <alissa.tophinke@hotmail.com>
2021-04-30 17:03:30 +0200

    task 3- long code

--- exercise2-week2.R
+++ exercise2-week2.R
@@ -16,7 +16,7 @@ wildschwein_BE <- read_delim("wildschwein_BE_2056.csv",",") # adjust path
 #setting remove = FALSE preserves the original (E/N) column:
 wildschwein_BE <- st_as_sf(wildschwein_BE, coords = c("E", "N"), crs = 2056, remove = FALSE)
 
-#TASK 1- getting an overview-------
+#####TASK 1- getting an overview-------
 #new column "timelag" using lead
 wildschwein_BE <- mutate(wildschwein_BE,timelag = as.numeric(difftime(lead(DatetimeUTC),
                                                                       DatetimeUTC,
@@ -66,7 +66,7 @@ ggplot(wildschwein_BE_14, aes(x=DatetimeUTC, y=timelag, color=TierID, na.re=TRUE
   geom_line()
 #What is the temporal sampling interval between the locations?
 
-#task 2 - Deriving movement parameters I: Speed------------
+#####task 2 - Deriving movement parameters I: Speed------------
 
 #calculate the Euclidean distance 
 wildschwein_BE <- group_by(wildschwein_BE,TierID)
@@ -81,7 +81,7 @@ wildschwein_BE <- mutate(wildschwein_BE,steplength = sqrt((E1-E2)^2+(N1-N2)^2))
 wildschwein_BE <- mutate(wildschwein_BE,speed = (steplength/timelag))
 ##ERROR occurs!! im not sure why??
 
-#try again with piping:
+# I try again with piping:
 wildschwein_BE <- wildschwein_BE %>%
   group_by(TierID) %>%
   mutate(timelag = as.numeric(difftime(lead(DatetimeUTC),DatetimeUTC,units = "secs")),
@@ -89,7 +89,7 @@ wildschwein_BE <- wildschwein_BE %>%
     speed = steplength/timelag)
 
 
-#Task 3: Cross-scale movement analysis------------------
+####Task 3: Cross-scale movement analysis------------------
 caro = read.delim("caro60.csv",sep=",",dec=".",header=TRUE)
 #Data cleansing
 #set timezone
@@ -230,3 +230,21 @@ geschw <- rbind(caro60, caro60_3, caro60_6, caro60_9)
 ggplot(geschw)+
   geom_line(mapping = aes(x=DatetimeUTC, y=speed, color=trajectory))
 
+###task 4- Deriving movement parameters II: Rolling window functions---------------
+library(zoo)
+#k is windowsize! 
+example <- rnorm(10)
+rollmean(example,k = 3,fill = NA,align = "left")
+##  [1]  0.93634335  0.31709038  0.02370048  0.67869801  0.73369105  0.50401344
+##  [7] -0.56144365 -0.56902598          NA          NA
+rollmean(example,k = 4,fill = NA,align = "left")
+##  [1]  0.6775521  0.2045005  0.5848215  0.5255629  0.3446928  0.1459635
+##  [7] -0.4102301         NA         NA         NA
+
+#now run rollmean on the speed variable of the subset (caro)
+
+x1 <- rollmean(caro60$speed, k=4, fill=NA, align="left")
+
+ggplot(x1)+
+  geom_line(mapping = aes(x=DatetimeUTC, y=speed, color=trajectory))
+         
